346466fa9070d7cd99878d003075c5019667e7ef
Louis Livingston <livingston.louis@gmail.com>
2023-07-09 19:14:18 -0400

    Made db configurable

--- src/app/database_query.py
+++ src/app/database_query.py
@@ -1,12 +1,19 @@
+import os
+
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_types import AgentType
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 
+DATABASE_TYPE = os.environ["NL_DATABASE_TYPE"]
+DATABASE_NAME = os.environ["NL_DATABASE_NAME"]
+
 
 class DatabaseQuery:
     def __init__(self) -> None:
-        self.db = SQLDatabase.from_uri("sqlite:///database/sqlite-sakila.db")
+        self.db = SQLDatabase.from_uri(
+            f"{DATABASE_TYPE}:///database/{DATABASE_NAME}"
+        )
         self.llm = OpenAI(temperature=0, verbose=False)
         self.toolkit = SQLDatabaseToolkit(db=self.db, llm=self.llm)
         self.agent_executor = create_sql_agent(
